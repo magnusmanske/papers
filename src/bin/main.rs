31967c9ba8ae548b2b67222afd79ae9c719ac2b8
Magnus Manske <magnusmanske@googlemail.com>
2019-03-14 16:26:06 +0000

    misc

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,33 @@ extern crate papers;
 
 //use crossref::Crossref;
 
+fn get_wikidata_item_for_doi(mw_api: &mediawiki::api::Api, doi: &String) -> Option<String> {
+    let sparql = format!(
+        "SELECT DISTINCT ?q {{ VALUES ?doi {{ '{}' '{}' '{}' }} . ?q wdt:P356 ?doi }}",
+        doi,
+        doi.to_uppercase(),
+        doi.to_lowercase()
+    ); // DOIs in Wikidata can be any upper/lowercase :-(
+    let res = match mw_api.sparql_query(&sparql) {
+        Ok(res) => res,
+        _ => return None,
+    };
+    let qs = mw_api.entities_from_sparql_result(&res, "q");
+
+    match qs.len() {
+        0 => None,
+        1 => Some(qs[0].clone()),
+        _ => {
+            println!(
+                "Multiple Wikidata items for DOI '{}' : {}",
+                &doi,
+                qs.join(", ")
+            );
+            None
+        }
+    }
+}
+
 fn main() {
     /*
         let mut settings = Config::default();
@@ -20,6 +47,7 @@ fn main() {
 
     let _ss_client = papers::semanticscholar::Client::new();
     let mw_api = mediawiki::api::Api::new("https://www.wikidata.org/w/api.php").unwrap();
+    let mut entities = mediawiki::entity_container::EntityContainer::new();
 
     let dois = vec!["10.1038/nrn3241"];
     for doi in dois {
@@ -28,31 +56,18 @@ fn main() {
                     Ok(work) => work,
                     _ => continue,
                 };
-                println!("{:?}", &work);
         */
-        let sparql = format!(
-            "SELECT DISTINCT ?q {{ VALUES ?doi {{ '{}' '{}' '{}' }} . ?q wdt:P356 ?doi }}",
-            doi,
-            doi.to_uppercase(),
-            doi.to_lowercase()
-        ); // DOIs in Wikidata can be any upper/lowercase :-(
-        let res = mw_api.sparql_query(&sparql).unwrap(); // TODO skip if fail
-        let qs = mw_api.entities_from_sparql_result(&res, "q");
-        match qs.len() {
-            0 => continue,
-            1 => {}
-            _ => {
-                println!(
-                    "Multiple Wikidata items for DOI '{}' : {}",
-                    &doi,
-                    qs.join(", ")
-                );
-                continue;
-            }
+        let q = match get_wikidata_item_for_doi(&mw_api, &doi.to_string()) {
+            Some(i) => i,
+            None => continue,
+        };
+        if entities.load_entities(&mw_api, &vec![q.clone()]).is_err() {
+            continue;
         }
-        let mut entities = mediawiki::entity_container::EntityContainer::new();
-        entities.load_entities(&mw_api, &qs).unwrap();
+        let item = match entities.get_entity(&q) {
+            Some(i) => i,
+            None => continue,
+        };
+        dbg!(item);
     }
-    //let work = papers::semanticscholar::Client::work("10.1037/0003-066X.59.1.29").unwrap();
-    //println!("{:?}", &work);
 }
