fad8484584e88fc4df7af7f83d7e3fd616a26430
Magnus Manske <magnusmanske@googlemail.com>
2026-03-05 13:16:26 +0000

    misc

--- restart.sh
+++ restart.sh
@@ -4,6 +4,6 @@ toolforge jobs delete rustbot
 # --image tool-sourcemd/tool-sourcemd:latest \
 # --command "sh -c 'target/release/main bot --config /data/project/sourcemd/rust/papers/bot.ini'" \
 # rustbot
-toolforge jobs run --mem 3000Mi --cpu 2 --continuous --mount=all --image tool-sourcemd/tool-sourcemd:latest \
+toolforge jobs run --mem 3000Mi --cpu 2 --mount=all --image tool-sourcemd/tool-sourcemd:latest \
 --command "sh -c 'target/release/main bot --config /data/project/sourcemd/rust/papers/bot.ini'" \
 rustbot
